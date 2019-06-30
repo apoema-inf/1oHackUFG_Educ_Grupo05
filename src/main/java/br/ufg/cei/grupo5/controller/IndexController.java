08146cce702ec818be116ac374f07192e08610e8
Unknown <ibruno.om@gmail.com>
2019-06-29 22:24:01 -0300

    Correcoes no layout

---
+++ src/main/java/br/ufg/cei/grupo5/controller/IndexController.java
@@ -0,0 +1,19 @@
+package br.ufg.cei.grupo5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Iago Bruno
+ * @since 1.0
+ */
+@Controller
+public class IndexController {
+
+  @RequestMapping({"/", ""})
+  public String index(){
+    return "/index";
+  }
+
+}
